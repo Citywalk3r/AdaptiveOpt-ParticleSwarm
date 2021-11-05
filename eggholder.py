1121720286980e516320a3b8059b164527a6cc31
Citywalk3r <minaspantelidakis@gmail.com>
2021-11-05 11:32:09 -0500

    Velocity boundaries

--- eggholder.py
+++ eggholder.py
@@ -36,24 +36,27 @@ class EGGHOLDER:
         data = []
 
         headers = ['iterations', 'swarm_size', 776, 12, 234, 9238, 123556, 59933, 98232, 85732, 5432, 12291]
-        seeds = [123556]
-        # seeds = [776, 12, 234, 9238, 123556, 59933, 98232, 85732, 5432, 12291]
+        # seeds = [123556]
+        seeds = [776, 12, 234, 9238, 123556, 59933, 98232, 85732, 5432, 12291]
         # iterations_list=[50,100,200,300]
-        iterations_list=[300]
+        iterations_list=[200]
         phi_list = [2.0, 2.0]
         # t_size_list=[9, 10, 11]
         # swarm_size_list=[100,200,400,800]
-        swarm_size_list=[800]
+        swarm_size_list=[250]
 
         for iterations in iterations_list:
             for idx, swarm_size in enumerate(swarm_size_list):
                 # plt.subplot(len(swarm_size_list)//2,3,idx+1)
                 # plt.title('swarm_size = {:}, iterations = {:}'.format(swarm_size, iterations))
+                # plt.xlabel('iterations')
+                # plt.ylabel('score')
                 best_per_seed = []
                 for seed in seeds:
                     best_list, _ = PS.pso(init_pos_f = self.generate_init_positions, swarm_size=swarm_size, iterations=iterations, eval_f=self.eval_func, 
                                         seed=seed, phi1=phi_list[0], phi2=phi_list[1],
-                                        neighborhood_topology="star", model="full", inertia=False, constriction=False, boundaries = [-512, 512])
+                                        neighborhood_topology="ring", model="full", inertia=False, constriction=False, 
+                                        boundaries = [[-512, 512], [-512,512]], velocity_restriction = True, generate_gifs=True)
 
                     # plt.plot(range(len(best_list)), best_list, label=str(seed))
                     # plt.legend()
@@ -67,9 +70,7 @@ class EGGHOLDER:
             # plt.show()
         df= pd.DataFrame(data=data, columns= list(map(str, headers)))
         print(df)
-        # df.to_excel("../ps_300it_800s_10seeds_ring.xlsx")
-       
-        # plt.show()
+        # df.to_excel("../ps_200it_250s_10seeds_cognition_star.xlsx")
 
 
 if __name__ == "__main__":
--- particle_swarm.py
+++ particle_swarm.py
@@ -18,10 +18,14 @@ class ParticleSwarm:
         return K
     
     def calculate_particle_velocity_limits(self, boundaries):
-        x_min = np.min(boundaries)
-        x_max = np.max(boundaries)
-        limit = (x_max-x_min)/2
-        return limit
+        limits = []
+        for boundary in boundaries:
+            x_min = np.min(boundary)
+            x_max = np.max(boundary)
+            limit = (x_max-x_min)/2
+            limits.append(limit)
+        print(limits)
+        return limits
     
     def create_particle_position_gif(self, particles, iterations, seed, model, neighborhood_topology):
         filenames = []
@@ -53,7 +57,7 @@ class ParticleSwarm:
             x_pos = []
             y_pos = []
         # build gif
-        with imageio.get_writer(f'./gifs/{seed}_{model}_{neighborhood_topology}.gif', mode='I') as writer:
+        with imageio.get_writer(f'./gifs/{seed}_{model}_{neighborhood_topology}_vrestriction.gif', mode='I') as writer:
             for filename in filenames:
                 image = imageio.imread(filename)
                 writer.append_data(image)
@@ -62,7 +66,9 @@ class ParticleSwarm:
         for filename in set(filenames):
             os.remove(filename)
 
-    def pso(self, init_pos_f, iterations, eval_f, seed, swarm_size, phi1, phi2, neighborhood_topology = "star", model="full", inertia=True, constriction=True, boundaries=None):
+    def pso(self, init_pos_f, iterations, eval_f, seed, 
+            swarm_size, phi1, phi2, neighborhood_topology = "star", model="full", 
+            inertia=True, constriction=True, boundaries=[[-512,512], [-512,512]], velocity_restriction=True, generate_gifs=False):
 
             """Particle Swarm Optimization algorithm
 
@@ -77,6 +83,8 @@ class ParticleSwarm:
                 model: "full", "cognition", "social", or "selfless"
                 inertia: Whether to apply intertia to the velocity vector or not.
                 constriction: Whether to apply constriction to the velocity vector or not.
+                boundaries: list of lists with function boundaries for each dimension.
+                velocity_restriction: Whether or not to restrict the max velocity of the particles.
 
             Returns:
                 best_list : list of historical best solutions through the iterations
@@ -94,13 +102,13 @@ class ParticleSwarm:
             if constriction:
                 K = self.calculate_constriction(phi1, phi2)
             
-            particle_velocity_limit = self.calculate_particle_velocity_limits(boundaries)
+            if velocity_restriction:
+                particle_velocity_limits = self.calculate_particle_velocity_limits(boundaries)
                
-            
             # Generate the particles
             initial_positions = init_pos_f(seed, swarm_size)
             for i in range(swarm_size):
-                swarm_list.append(Particle(self.is_debug, initial_positions[i], eval_f, phi1, phi2, model, particle_velocity_limit))
+                swarm_list.append(Particle(self.is_debug, initial_positions[i], eval_f, phi1, phi2, model, velocity_restriction))
             
             if neighborhood_topology == "ring":
                 for particle in swarm_list:
@@ -130,7 +138,7 @@ class ParticleSwarm:
                 best_list.append(best_particle.p_fitness)
 
                 for particle in swarm_list:
-                    particle.adjust_v(best_particle, ω, K, second_best_particle)
+                    particle.adjust_v(best_particle, ω, K, second_best_particle, particle_velocity_limits)
                     particle.move()
                 
                 if inertia:
@@ -139,17 +147,17 @@ class ParticleSwarm:
             random_particles = sample(swarm_list,2)
             print("Final interia: ", ω)
     
-            if seed == 123556:
+            if generate_gifs:
                 self.create_particle_position_gif(swarm_list, iterations, seed, model, neighborhood_topology)
 
             return best_list, random_particles
 
 class Particle:
 
-    def __init__(self, is_debug, position, eval_f, phi1, phi2, model, v_limit):
+    def __init__(self, is_debug, position, eval_f, phi1, phi2, model, velocity_restriction):
         self.is_debug = is_debug
         self.model = model
-        self.v_limit = v_limit
+        self.velocity_restriction = velocity_restriction
         self.historical_v = []
         self.historical_x = []
         self.eval_f = eval_f
@@ -157,8 +165,8 @@ class Particle:
         self.p_vector = self.x_vector
         self.v_vector = self.initialize_v_vector(len(position)) # Gradient
         self.historical_v.append(self.v_vector)
-        self.x_fitness = 999999999999
-        self.p_fitness = 999999999999
+        self.x_fitness = 1e50
+        self.p_fitness = 1e50
         self.neighbors = None
 
         if model == "social" or model == "selfless":
@@ -177,8 +185,6 @@ class Particle:
             tmp = self.x_vector[i] + self.v_vector[i]
             if -512 <= tmp <= 512:
                 self.x_vector[i] = tmp
-            # else:
-            #     self.v_vector[i]=0
 
         
         
@@ -191,7 +197,7 @@ class Particle:
         # print(self.p_fitness)
         self.historical_x.append(self.x_vector.copy())
 
-    def adjust_v(self, best_particle, ω, K, second_best_particle):
+    def adjust_v(self, best_particle, ω, K, second_best_particle, limits):
 
         if self.neighbors:
             best_neighbor = sorted(self.neighbors, key=lambda particle: particle.p_fitness)[0]
@@ -216,7 +222,14 @@ class Particle:
                                     self.phi1*np.random.uniform(0,1)*(self.p_vector[i] - self.x_vector[i]) + 
                                     self.phi2*np.random.uniform(0,1)*(second_best_particle.p_vector[i] - self.x_vector[i])
                                     for i in range(len(self.x_vector))]
-
+        
+        if self.velocity_restriction:
+            for i in range(len(self.v_vector)):
+                if self.v_vector[i] < -limits[i]:
+                    self.v_vector[i] = self.v_vector[i]/limits[i]
+                elif self.v_vector[i] > limits[i]:
+                    self.v_vector[i] = self.v_vector[i]/limits[i]
+        
         self.historical_v.append(self.v_vector)
 
         
@@ -251,7 +264,8 @@ if __name__ == "__main__":
     PSO = ParticleSwarm(is_debug=False)
     best_list, random_particles = PSO.pso(init_pos_f=generate_init_positions, iterations=120, 
                                         eval_f=eval_func, seed=776, swarm_size=500, phi1=2.0, phi2=2.0,
-                                        neighborhood_topology = "star", model="full", inertia=False, constriction=False)
+                                        neighborhood_topology = "star", model="full", inertia=False, constriction=False,
+                                        boundaries=[[-512,512], [-512,512]], velocity_restriction=True)
 
     fig, ax = plt.subplots(1,2)
     fig.text(0.5, 0.04, 'iterations', ha='center', fontsize=12)
@@ -261,7 +275,7 @@ if __name__ == "__main__":
         ax.flatten()[idx].set_title('Particle {}'.format(idx), fontsize=12)
         ax.flatten()[idx].plot(range(len(x_v)),x_v, label="x velocity")
         ax.flatten()[idx].plot(range(len(y_v)),y_v, label="y velocity")
-        ax.flatten()[idx].set_yscale('log')
+        # ax.flatten()[idx].set_yscale('log')
         ax.flatten()[idx].legend()
 
     plt.show()
